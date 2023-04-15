46407c68fd69f88af2662ad471734b15cdc6ca87
camenduru <54370274+camenduru@users.noreply.github.com>
2023-04-15 11:07:12 +0300

    install.py

---
+++ install.py
@@ -0,0 +1,10 @@
+import launch
+
+if not launch.is_installed("ffmpeg-python"):
+    launch.run_pip("install ffmpeg-python", "requirements for TemporalKit extension")
+
+if not launch.is_installed("moviepy"):
+    launch.run_pip("install moviepy", "requirements for TemporalKit extension")
+
+if not launch.is_installed("tensorflow"):
+    launch.run_pip("install tensorflow", "requirements for TemporalKit extension")
