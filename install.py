bbfa69878ac05df1585e462709e1d5e9793776b9
Strawberries <crowles98@gmail.com>
2023-04-17 09:15:10 +0100

    imageio_ffmpeg fix

--- install.py
+++ install.py
@@ -1,8 +1,12 @@
 import launch
 
+#todo: check if ffmpeg-python is used anywhere, could have sworn i removed it but not 100% enough to change now.
 if not launch.is_installed("ffmpeg-python"):
     launch.run_pip("install ffmpeg-python", "requirements for TemporalKit extension")
 
 if not launch.is_installed("moviepy"):
     launch.run_pip("install moviepy", "requirements for TemporalKit extension")
+    
+if not launch.is_installed("imageio_ffmpeg"):
+    launch.run_pip("install imageio_ffmpeg", "requirements for TemporalKit extension")
 
