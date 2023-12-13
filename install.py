6a6f96446a68f690a24177deedcad2d8486b070f
cocomine <roguish.6888467468@gmail.com>
2023-12-13 16:29:36 +0800

    tqdm

--- install.py
+++ install.py
@@ -5,7 +5,7 @@ if not launch.is_installed("ffmpeg"):
 
 if not launch.is_installed("moviepy"):
     launch.run_pip("install moviepy", "Install \"moviepy\" requirements for TemporalKit extension")
-    launch.run_pip("install tqdm>=4.66.1", "Re-Install \"tqdm\" to version >=4.66.1 for TemporalKit extension")
+    launch.run_pip("install tqdm==4.66.1", "Re-Install \"tqdm\" to version >=4.66.1 for TemporalKit extension")
 
 if not launch.is_installed("imageio_ffmpeg"):
     launch.run_pip("install imageio_ffmpeg", "Install \"imageio_ffmpeg\" requirements for TemporalKit extension")
